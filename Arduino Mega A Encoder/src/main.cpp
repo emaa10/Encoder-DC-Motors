8e7ebf73cf5853a88658544ff5381bf1ed432e67
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-21 18:24:34 +0100

    debugging

--- Arduino Mega A Encoder/src/main.cpp
+++ Arduino Mega A Encoder/src/main.cpp
@@ -6,8 +6,8 @@ const int LEFT_ENC_B_PHASE = 2;
 const int RIGHT_ENC_A_PHASE = 18;
 const int RIGHT_ENC_B_PHASE = 19;
 
-volatile long leftEncoderCount = 0;
-volatile long rightEncoderCount = 0;
+volatile float leftEncoderCount = 0;
+volatile float rightEncoderCount = 0;
 
 void leftEncoderISR() {
   if (digitalRead(LEFT_ENC_B_PHASE) == HIGH) {
@@ -26,10 +26,10 @@ void rightEncoderISR() {
 }
 
 void setup() {
-  pinMode(LEFT_ENC_A_PHASE, INPUT);
-  pinMode(LEFT_ENC_B_PHASE, INPUT);
-  pinMode(RIGHT_ENC_A_PHASE, INPUT);
-  pinMode(RIGHT_ENC_B_PHASE, INPUT);
+  pinMode(LEFT_ENC_A_PHASE, INPUT_PULLUP);
+  pinMode(LEFT_ENC_B_PHASE, INPUT_PULLUP);
+  pinMode(RIGHT_ENC_A_PHASE, INPUT_PULLUP);
+  pinMode(RIGHT_ENC_B_PHASE, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(LEFT_ENC_A_PHASE), leftEncoderISR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_A_PHASE), rightEncoderISR, CHANGE);
