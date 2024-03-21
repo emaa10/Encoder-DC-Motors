9772d280e3ad90243f79fd43777312110e0f0431
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-21 21:13:17 +0100

    fix

--- Arduino Mega A Encoder/src/main.cpp
+++ Arduino Mega A Encoder/src/main.cpp
@@ -8,31 +8,63 @@ const int LEFT_ENC_B_PHASE = 2;
 const int RIGHT_ENC_A_PHASE = 18;
 const int RIGHT_ENC_B_PHASE = 19;
 
+// LEFT_1
 void ai0()
 {
   // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
   // Check pin 3 to determine the direction
-  if (digitalRead(3) == LOW)
+  if (digitalRead(LEFT_ENC_A_PHASE) == LOW)
   {
-    counter++;
+    counterLEFT++;
   }
   else
   {
-    counter--;
+    counterLEFT--;
   }
 }
 
+// LEFT_2
 void ai1()
 {
   // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
   // Check with pin 2 to determine the direction
-  if (digitalRead(2) == LOW)
+  if (digitalRead(LEFT_ENC_B_PHASE) == LOW)
   {
-    counter--;
+    counterLEFT--;
   }
   else
   {
-    counter++;
+    counterLEFT++;
+  }
+}
+
+// RIGHT_1
+void bi0()
+{
+  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
+  // Check pin 3 to determine the direction
+  if (digitalRead(RIGHT_ENC_A_PHASE) == LOW)
+  {
+    counterRIGHT++;
+  }
+  else
+  {
+    counterRIGHT--;
+  }
+}
+
+// RIGHT_2
+void bi1()
+{
+  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
+  // Check with pin 2 to determine the direction
+  if (digitalRead(RIGHT_ENC_B_PHASE) == LOW)
+  {
+    counterRIGHT--;
+  }
+  else
+  {
+    counterRIGHT++;
   }
 }
 
@@ -40,19 +72,14 @@ void setup()
 {
   Serial.begin(9600);
 
-  pinMode(3, INPUT_PULLUP); // internal pullup input pin 2
-
-  pinMode(2, INPUT_PULLUP); // internalเป็น pullup input pin 3
-                            // Setting up interrupt
-  // A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
+  pinMode(LEFT_ENC_A_PHASE, INPUT_PULLUP); 
+  pinMode(LEFT_ENC_B_PHASE, INPUT_PULLUP); 
   attachInterrupt(0, ai0, RISING);
-
-  // B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
   attachInterrupt(1, ai1, RISING);
 }
 
 void loop()
 {
-  Serial.println(counter);
+  Serial.println(counterLEFT);
   delay(20);
 }
