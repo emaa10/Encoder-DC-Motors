b20521920e91f2e18ea9d411520c864ce41ec1bf
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-21 17:59:49 +0100

    asdf

--- Arduino Mega A Encoder/src/main.cpp
+++ Arduino Mega A Encoder/src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Encoder pins
+const int LEFT_ENC_A_PHASE = 3;
+const int LEFT_ENC_B_PHASE = 2;
+const int RIGHT_ENC_A_PHASE = 18;
+const int RIGHT_ENC_B_PHASE = 19;
+
+volatile long leftEncoderCount = 0;
+volatile long rightEncoderCount = 0;
+
+void leftEncoderISR() {
+  if (digitalRead(LEFT_ENC_B_PHASE) == HIGH) {
+    leftEncoderCount++;
+  } else {
+    leftEncoderCount--;
+  }
+}
+
+void rightEncoderISR() {
+  if (digitalRead(RIGHT_ENC_B_PHASE) == HIGH) {
+    rightEncoderCount--;
+  } else {
+    rightEncoderCount++;
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LEFT_ENC_A_PHASE, INPUT);
+  pinMode(LEFT_ENC_B_PHASE, INPUT);
+  pinMode(RIGHT_ENC_A_PHASE, INPUT);
+  pinMode(RIGHT_ENC_B_PHASE, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(LEFT_ENC_A_PHASE), leftEncoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_A_PHASE), rightEncoderISR, CHANGE);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Send encoder counts to Raspberry Pi
+  Serial.print("LeftEncoder:");
+  Serial.print(leftEncoderCount);
+  Serial.print(",RightEncoder:");
+  Serial.println(rightEncoderCount);
+  delay(100); // Adjust delay as needed
 }
