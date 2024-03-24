109c3577b852b620761b808a37b8c2b7b1006817
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-24 15:55:06 +0100

    building code

--- raspi/main/main.cpp
+++ raspi/main/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
-int main() {
-    std::cout << "hi";
-    return 0;
+const std::string serialMegaA = "/dev/ttyACM0";
+
+void getEncoderData() {
+    //
 }
 
+void setup() {
+    // initialize wiringpi
+    if (wiringPiSetup() == -1) {
+        std::cerr << "Fehler beim Initialisieren von WiringPi." << std::endl;
+    }
+    int serial_port = serialOpen(serialMegaA.c_str(), 9600);
+    if (serial_port < 0) {
+        std::cerr << "Port error on Mega A Encoder, Port " << serialMegaA << std::endl;
+    }
+}
+
+void loop() {
+    //
+}
+
+
+
+// ---
+
+int main() {
+    setup();
+    while(true) {loop();}
+}
