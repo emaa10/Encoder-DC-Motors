3bb0ba3134b401be951b5314be88bbcebbc36d17
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-24 16:39:22 +0100

    encoder reader

--- raspi/encoder_reader/encoder_reader.cpp
+++ raspi/encoder_reader/encoder_reader.cpp
@@ -16,15 +16,15 @@ int main() {
     }
 
     std::string line;
-    long int float1, float2;
+    long int encLeft, encRight;
     char comma;
     while (true) {
         auto start_time = std::chrono::steady_clock::now();
         while (std::chrono::steady_clock::now() - start_time < std::chrono::milliseconds(100)) {
             if (std::getline(serial, line)) {
                 std::istringstream iss(line);
-                if (iss >> float1 >> comma >> float2) {
-                    std::cout << "Float 1: " << float1 << ", Float 2: " << float2 << std::endl;
+                if (iss >> encLeft >> comma >> encRight) {
+                    std::cout << "Encoder Left: " << encLeft << ", Encoder Right: " << encRight << std::endl;
                 }
             }
         }
