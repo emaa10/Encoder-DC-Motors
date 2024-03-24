e635bb32e62fd94654bcbe224d6713992a6afe6b
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-24 12:32:28 +0100

    just drive code

--- Arduino Mega B Motor/src/main.cpp
+++ Arduino Mega B Motor/src/main.cpp
@@ -1,55 +1,42 @@
-// nur pwm getten und auf motor packen
 #include <Arduino.h>
 
-// left dc motor pins
-#define LEFT_DCM_LEFT_PWM 5
-#define LEFT_DCM_RIGHT_PWM 4
+   /*
+  BTS7960-43A-Driver
+  made on 22 Nov 2020
+  by Amir Mohammad Shojaee @ Electropeak
+  Home
+*/
 
-// right dc motor pins
-#define RIGHT_DCM_LEFT_PWM 2
-#define RIGHT_DCM_RIGHT_PWM 3
+#define LEFT_LPWM 5
+#define LEFT_RPWM 4
+#define RIGHT_LPWM 2
+#define RIGHT_RPWM 3
 
-float pwmLeft=0;
-float pwmRight=0;
-
-float receivedValue1; // pwm value left
-float receivedValue2; // pwm value right
+int outLeft;
+int outRight;
 
 void setup() {
-  // Beginnen der seriellen Kommunikation mit einer Baudrate von 9600
   Serial.begin(9600);
-  // pinmodes
-  pinMode(LEFT_DCM_LEFT_PWM, OUTPUT);
-  pinMode(LEFT_DCM_RIGHT_PWM, OUTPUT);
-  pinMode(RIGHT_DCM_LEFT_PWM, OUTPUT);
-  pinMode(RIGHT_DCM_RIGHT_PWM, OUTPUT);
-}
+  pinMode(RIGHT_RPWM,OUTPUT);
+  pinMode(RIGHT_LPWM,OUTPUT);
 
+  pinMode(LEFT_RPWM,OUTPUT);
+  pinMode(LEFT_LPWM,OUTPUT);
+
+  outLeft = 100;
+  outRight = 100;
+  
+
+}
+ 
+ 
 void loop() {
-  // Überprüfen, ob Daten auf der seriellen Schnittstelle verfügbar sind
-  if (Serial.available() > 0) {
-    // Lesen der empfangenen Daten bis zum Komma und Speichern in der ersten Float-Variablen
-    receivedValue1 = Serial.parseFloat();
-    
-    // Warten auf das Komma
-    while (Serial.read() != ',');
-    
-    // Lesen der empfangenen Daten nach dem Komma und Speichern in der zweiten Float-Variablen
-    receivedValue2 = Serial.parseFloat();
-    
-    // Warten auf das Ende der aktuellen Zeile, um sicherzustellen, dass keine weiteren Zeichen im Puffer sind
-    while (Serial.read() != '\n');
-    
-    pwmLeft = receivedValue1;
-    pwmRight = receivedValue2;
-    analogWrite(RIGHT_DCM_LEFT_PWM, pwmRight);
-    analogWrite(RIGHT_DCM_RIGHT_PWM, 0);
-    analogWrite(LEFT_DCM_LEFT_PWM, pwmLeft);
-    analogWrite(LEFT_DCM_RIGHT_PWM, 0);
-    Serial.print("Received value 1: ");
-    Serial.println(receivedValue1);
-    Serial.print("Received value 2: ");
-    Serial.println(receivedValue2);
-  }
-  delay(10);
+  
+  
+  analogWrite(RIGHT_LPWM,outRight);
+  analogWrite(RIGHT_RPWM,0);
+  analogWrite(LEFT_LPWM,outLeft);
+  analogWrite(LEFT_RPWM,0);
+  delay(1000);
+
 }
