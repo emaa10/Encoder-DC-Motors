22b830731498babb57c6df28f555728b4f3850b1
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-24 19:38:40 +0100

    grab enc data

--- raspi/main/main.cpp
+++ raspi/main/main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <unistd.h>
 #include <chrono>
 #include "wiringPi.h"
 #include "wiringSerial.h"
 
 
-const std::string serialMegaA = "/dev/ttyACM0"; // needs change
-const std::string serialMegaB = "/dev/ttyACM1";
+const std::string serialMegaA = "/dev/ttyACM0"; // enc
+const std::string serialMegaB = "/dev/ttyACM1"; // dc
 std::ifstream serial(serialMegaA.c_str());
 int sPortB = serialOpen(serialMegaB.c_str(), 9600);
 
+long int encoderLeft; // enc count left
+long int encoderRight;// enc count right
+
+float currentPwmLeft;
+float currentPwmRight;
+
 void getEncoderData() {
-    //
+    std::string line;
+    char comma;
+    auto start_time = std::chrono::steady_clock::now();
+    while (std::chrono::steady_clock::now() - start_time < std::chrono::milliseconds(100)) {
+        if (std::getline(serial, line)) {
+            std::istringstream iss(line);
+            if (iss >> encoderLeft >> comma >> encoderRight) {
+                std::cout << "Encoder Left: " << encoderLeft << ", Encoder Right: " << encoderRight << std::endl;
+            }
+        }
+    }
 }
 
 void sendPWMValues(float pwmLeft, float pwmRight) {
+    currentPwmLeft = pwmLeft;
+    currentPwmRight = pwmRight;
     std::string message = std::to_string(pwmLeft) + "," + std::to_string(pwmRight);
     serialPrintf(sPortB, "%s\n", message.c_str());
 }
@@ -37,7 +56,9 @@ void setup() {
 }
 
 void loop() {
-    //
+    getEncoderData();
+    std::cout << encoderLeft << std::endl;
+    delay(50);
 }
 
 
