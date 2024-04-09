1ec417442e29ae5bc9f7bcd218fb82ef7adb1906
nikolai.ivanov <nikolai.ivanov@uni-konstanz.de>
2024-04-09 10:08:39 +0200

    Added freeBack freeTurn check

--- raspi/eurobot-lidar/lidar.h
+++ raspi/eurobot-lidar/lidar.h
@@ -111,6 +111,60 @@ public:
         return true;
     }
 
+    bool freeTurn(RobotPose current_pos) {
+        sl_lidar_response_measurement_node_hq_t nodes[8192];
+        size_t count = _countof(nodes);
+        sl_result result = drv->grabScanDataHq(nodes, count);
+        if (SL_IS_OK(result)) {
+            drv->ascendScanData(nodes, count);
+            for (int pos = 0; pos < (int)count ; ++pos) {
+                Vector point;
+                //Calculate point
+                double angle = -(nodes[pos].angle_z_q14 * 90.f) / 16384.f - current_pos.angle;
+                int distance = nodes[pos].dist_mm_q2/4.0f;
+                
+                double angle_rad = angle * M_PI / 180.0;
+                point.x = distance * cos(angle_rad);
+                point.y = distance * sin(angle_rad);
+                point += current_pos.position;
+
+                //Check if point is inside field
+                if (distance < 10 || point.x < 10 || point.x > 2990 || point.y < 10 || point.y > 1990) continue;
+
+                //Check if enemy is near
+                if (distance < 40) return false;
+            }
+        }
+        return true;
+    }
+
+    bool freeBack(RobotPose current_pos) {
+        sl_lidar_response_measurement_node_hq_t nodes[8192];
+        size_t count = _countof(nodes);
+        sl_result result = drv->grabScanDataHq(nodes, count);
+        if (SL_IS_OK(result)) {
+            drv->ascendScanData(nodes, count);
+            for (int pos = 0; pos < (int)count ; ++pos) {
+                Vector point;
+                //Calculate point
+                double angle = -(nodes[pos].angle_z_q14 * 90.f) / 16384.f - current_pos.angle;
+                int distance = nodes[pos].dist_mm_q2/4.0f;
+                
+                double angle_rad = angle * M_PI / 180.0;
+                point.x = distance * cos(angle_rad);
+                point.y = distance * sin(angle_rad);
+                point += current_pos.position;
+
+                //Check if point is inside field
+                if (distance < 10 || point.x < 10 || point.x > 2990 || point.y < 10 || point.y > 1990) continue;
+
+                //Check if enemy is near
+                if (distance < 40 && (angle > 120 && angle < 240)) return false;
+            }
+        }
+        return true;
+    }
+
     void stopLidar() {
         drv->stop();
         drv->disconnect();
