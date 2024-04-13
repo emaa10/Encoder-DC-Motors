9a97877b728e236514a81d5859c436cfa30c9005
Emanuel Berger <emanuelberger49@gmail.com>
2024-04-13 18:04:19 +0200

    moving avarage

--- Arduino Mega/src/main.h
+++ Arduino Mega/src/main.h
@@ -20,8 +20,7 @@ const float wheelDistance = 128; //abstand der encoderräder in mm, muss vllt ge
 const float wheelDistanceBig = 204; // in mm, muss vllt geändert werden
 const float turnValue = wheelDistance * M_PI / 360; // abstand beider räder um 1° zu fahren
 
-const int syncInterval = 1; // sync motors with encoders every second
-const int syncCounter = syncInterval * 1000 / 5;
+const int syncCounter = 50;
 const int syncCounterTurn = 50; // check alle 200ms, wenn ich das änder auch das /5 beim turn ändern!
 
 extern float currentPwmLeft;
