765c849a79d668113f29132fba522d44256f972b
Emanuel Berger <emanuelberger49@gmail.com>
2024-04-13 17:40:53 +0200

    refresh

--- Arduino Mega/src/main.cpp
+++ Arduino Mega/src/main.cpp
@@ -1,298 +1,247 @@
+#include "simplepid.h"
 #include <Arduino.h>
-#include "./main.h"
+#include <util/atomic.h>
+
 // Define pins
 
-String debug = "";
+#define LEFT_ENC_A_PHASE 18
+#define LEFT_ENC_B_PHASE 19
+#define RIGHT_ENC_A_PHASE 2
+#define RIGHT_ENC_B_PHASE 3
 
-float currentPwmLeft;
-float currentPwmRight;
+const int lpwm[] = {9, 11};
+const int rpwm[] = {8, 10};
+String DEBUG = "";
 
-bool driving = false;
+// Define Globals
+
+#define NMOTORS 2
+#define pwmCutoff 20 // Set minimum drivable pwm value
+#define pulsesCutoff 3
+#define pwmMax 70
+int currentPwm = pwmMax;
+long prevT = 0;
+volatile int posi[] = {0, 0};
+int lastPos[] = {0, 0};
+SimplePID pid[NMOTORS];
+int target[] = {0, 0};
+
+const float pulsesPerEncRev = 1200;
+const float encWheelDiameterCM = 5;
+const float motorWheelDiameterCM = 7;
+const float encWheelScope = encWheelDiameterCM * M_PI;
+const float motorWheelScope =
+    motorWheelDiameterCM * M_PI; // distance travelled per rev
+const float pulsesPerRev = pulsesPerEncRev * (motorWheelScope / encWheelScope);
+const float pulsesPerMM = pulsesPerRev / motorWheelScope / 10;
+const float pulsesPerCM = pulsesPerRev / motorWheelScope;
+const float pwmSpeed = 100; // default pwm speed
+const float pulsesPerSec =
+    pulsesPerRev; // goal pulses per sec 1680, 1 round per second
+const float wheelDistance =
+    130.3551558; // abstand der encoderräder in mm, muss vllt geändert werden
+const float wheelDistanceBig = 204; // in mm, muss vllt geändert werden
+// const float turnValue =
+//     wheelDistance * M_PI / 360; // abstand beider räder um 1° zu fahren
+
+const float pulsesValue = pulsesPerMM;
 
 float x = 0;
 float y = 0;
 float theta = 0;
-long int lastEncLeft=0; // maybe not needed
-long int lastEncRight=0;
 
-volatile long int encoderLeft=0;
-volatile long int encoderRight=0;
+float extrax = 0;
+float extray = 0;
+float extraTheta = 0;
 
-int counter=0;
-unsigned long previousMillis = 0;
-const long updatePosInterval = 1000;
+bool isDriving = false;
 
-long int oldEncoderLeft;
-long int oldEncoderRight;
-
-long int leftEncoderChange;
-long int rightEncoderChange;
-
-float leftEnc1 = 0;
-float rightEnc1 = 0;
-float leftEnc2 = 0;
-float rightEnc2 = 0;
-
+long lastPosUpdate;
+bool leftTriggered = true;
+bool rightTriggered = true;
+// Encoder read functions
 
 void ai0() {
-  if (digitalRead(LEFT_ENC_A_PHASE) == LOW) { encoderLeft++; }
-  else { encoderLeft--; }
+  if (digitalRead(LEFT_ENC_A_PHASE) == LOW) {
+    posi[1]++;
+  } else {
+    posi[1]--;
+  }
 }
 
 void ai1() {
-  if (digitalRead(LEFT_ENC_B_PHASE) == LOW) { encoderLeft--; }
-  else { encoderLeft++; }
+  if (digitalRead(LEFT_ENC_B_PHASE) == LOW) {
+    posi[1]--;
+  } else {
+    posi[1]++;
+  }
 }
 
 void bi0() {
-  if (digitalRead(RIGHT_ENC_A_PHASE) == LOW) { encoderRight++; }
-  else { encoderRight--; }
+  if (digitalRead(RIGHT_ENC_A_PHASE) == LOW) {
+    posi[0]++;
+  } else {
+    posi[0]--;
+  }
 }
 
 void bi1() {
-  if (digitalRead(RIGHT_ENC_B_PHASE) == LOW) { encoderRight--; }
-  else { encoderRight++; }
-}
-
-
-long int getEncoderLeft() {
-  return encoderLeft;
-}
-
-long int getEncoderRight() {
-  return encoderRight;
-}
-
-void stopMotor() {setPwmValues(0, 0); }
-
-void setEncoderZero() {encoderLeft=0; encoderRight=0;}
-
-float getAngle(float input = theta) {
-    float result = theta*180/M_PI;
-    // result = fmod((result + 360.0), 360.0);
-    // now in updatepos with theta
-    return result;
-}
-
-void setPwmValues(float pwmLeft, float pwmRight) {
-  float pL = abs(pwmLeft);
-  float pR = abs(pwmRight);
-  currentPwmLeft=pwmLeft;
-  currentPwmRight=pwmRight;
-
-  if(pwmLeft < 0) { // wenn wir links rückwärts fahren wollen
-    analogWrite(LEFT_LPWM, 0);
-    analogWrite(LEFT_RPWM, pL);
-  } else{ // wenn wir vorwärts fahren wollen
-    analogWrite(LEFT_LPWM, pL);
-    analogWrite(LEFT_RPWM, 0);
+  if (digitalRead(RIGHT_ENC_B_PHASE) == LOW) {
+    posi[0]--;
+  } else {
+    posi[0]++;
   }
+}
 
-  if(pwmRight < 0) {  // wenn wir rechts rückwärts fahren wollen
-    analogWrite(RIGHT_LPWM, 0);
-    analogWrite(RIGHT_RPWM, pR);
-  } else{
-    analogWrite(RIGHT_LPWM, pR);
-    analogWrite(RIGHT_RPWM, 0);
+// PID program functions
+
+void setMotor(int dir, int pwmVal, int lpwm, int rpwm) {
+  if (dir == 1) {
+    analogWrite(lpwm, pwmVal);
+  } else if (dir == -1) {
+    analogWrite(rpwm, pwmVal);
+  } else {
+    analogWrite(lpwm, 0);
+    analogWrite(rpwm, 0);
   }
-  driving = !(pwmLeft == 0 && pwmRight == 0);
 }
 
-void drive(float drivePwmLeft, float drivePwmRight) {
-    if(drivePwmLeft > 150) {drivePwmLeft = 150;}
-    if(drivePwmRight > 150) {drivePwmRight = 150;}
-    if(drivePwmLeft < -150) {drivePwmLeft = -150;}
-    if(drivePwmRight < -150) {drivePwmRight = -150;}
-    setPwmValues(drivePwmLeft, drivePwmRight);
-};
+void updatePosition();
+void resetPosition() {
+  updatePosition();
 
-void updatePosition(float leftEncChange, float rightEncChange) {
-    float leftDistance = leftEncChange / pulsesPerMM;
-    float rightDistance = rightEncChange / pulsesPerMM;
-    float distance = (leftDistance + rightDistance) / 2;
-    float dTheta = (rightDistance - leftDistance) / wheelDistance;
-    x += distance * cos(theta + dTheta / 2);
-    y += distance * sin(theta + dTheta / 2);
-    theta += dTheta;
-    theta = fmod((theta + 2 * M_PI), (2 * M_PI)); // test in radian
-    sendData();
+  x += extrax;
+  y += extray;
+  extrax = 0;
+  extray = 0;
+  theta += extraTheta;
+  while (theta > 2 * M_PI) {
+    theta -= 2 * M_PI;
+  }
+  while (theta < -2 *M_PI) {
+    theta += 2 * M_PI;
+  }
+  extraTheta = 0;
+
+  lastPos[0] = 0;
+  lastPos[1] = 0;
+  posi[0] = 0;
+  posi[1] = 0;
+  target[0] = 0;
+  target[1] = 0;
 }
 
-void updatePositionThread() { // NEED MILLIS
-    unsigned long currentMillis = millis();
-    if(currentMillis - previousMillis >= 100) {
-      previousMillis = currentMillis;
-
-
-      leftEnc2 = getEncoderLeft();
-      rightEnc2 = getEncoderRight();
-      updatePosition(leftEnc2 - leftEnc1, rightEnc2 - rightEnc1);
-      leftEnc1 = getEncoderLeft();
-      rightEnc1 = getEncoderRight();
-    }
-}
-
-
-
-void turn(float degrees) {
-    float distance = turnValue * degrees; // in mm
-    float pulsesLeft = -1.0f * (distance * pulsesPerMM); // links rückwärts... sollte passen ig
-    float pulsesRight = distance * pulsesPerMM;
-
-    int startEncLeft = getEncoderLeft();
-    int startEncRight = getEncoderRight();
-    int lastEncLeft = getEncoderLeft();
-    int lastEncRight = getEncoderRight();
-    long int currentEncoderLeft = 0;
-    long int currentEncoderRight = 0;
-    long int currentPIDleft = 0;
-    long int currentPIDright = 0;
-
-    drive(-pwmSpeed, pwmSpeed);
-    counter = 0;
-    while((abs(currentEncoderLeft)+abs(currentEncoderRight))/2 < (abs(pulsesLeft)+abs(pulsesRight))/2) { // solange wir noch nicht da sind
-        currentEncoderLeft = getEncoderLeft() - startEncLeft;
-        currentEncoderRight = getEncoderRight() - startEncRight;
-        currentPIDleft = getEncoderLeft() - lastEncLeft;
-        currentPIDright = getEncoderRight() - lastEncRight;
-        // check ob gegner auf stregge brauchen wir hier nicht
-        counter++;
-        // debug = "PULSES NEEDED IN TIME: " + String(pulsesPerSec/(1000/syncCounterTurn));
-        if(counter >= syncCounterTurn) {
-            if(currentEncoderLeft != 0 && currentEncoderRight != 0) { // fehler vermeiden
-                float newPwmLeft = (pulsesPerSec/(1000/syncCounterTurn) / abs(currentPIDleft) * currentPwmLeft); // geteilt durch 5 wegen syncCounterTurn
-                float newPwmRight = (pulsesPerSec/(1000/syncCounterTurn) / abs(currentPIDright) * currentPwmRight);
-                debug = String(newPwmLeft) +  " " + String(newPwmRight) + " , " + String(currentPIDleft) + " " + String(currentPIDright);
-                drive(newPwmLeft, newPwmRight);
-                lastEncLeft = getEncoderLeft();
-                lastEncRight = getEncoderRight();
-                // odom calc start
-                // updatePosition(currentPIDleft, currentPIDright);
-                // odom calc end
-                getData();
-                updatePositionThread();
-            }
-            counter = 0;
-            getData();
-            updatePositionThread();
-        }
-        delay(1);
-        updatePositionThread();
-        getData();
-    }
-    drive(0, 0);
-    // updatePosition(currentPIDleft, currentPIDright);
-    // odom manual start -> not recommended
-    // theta += degrees;
-    // theta = fmod((theta + 360.0), 360.0);
-    // odom manual end
-}
+// Driving functions
 
 void driveDistance(int distance) {
-    // RUN BEFORE DRIVING!!
-    int startEncLeft = getEncoderLeft();
-    int startEncRight = getEncoderRight();
-    int lastEncLeft = getEncoderLeft();
-    int lastEncRight = getEncoderRight();
+  resetPosition();
 
-    float distancePulses = distance * pulsesPerMM;
-
-    // need these 2 lines to recalculate current enc values. 
-    long int currentEncoderLeft = 0; // for driving
-    long int currentEncoderRight = 0;
-    long int currentPIDleft = 0;
-    long int currentPIDright = 0;
-
-    drive(pwmSpeed, pwmSpeed); // start with 100 pwm
-    counter = 0;
-    while(distancePulses > (currentEncoderLeft + currentEncoderRight)/2) { // might need correction
-        // solange wir noch nicht da sind
-        currentEncoderLeft = getEncoderLeft() - startEncLeft;
-        currentEncoderRight = getEncoderRight() - startEncRight;
-        currentPIDleft = getEncoderLeft() - lastEncLeft;
-        currentPIDright = getEncoderRight() - lastEncRight;
-        // hier check ob gegner auf strecke
-        counter++;
-        if(counter >= syncCounter) { //wenn bestimmte zeit vergangen
-            // neue pwm werte basierend auf encoder daten berechnen und positionsbestimmung
-            if(currentEncoderLeft != 0 && currentEncoderRight != 0) {
-                float newPwmLeft = currentPwmLeft * currentPIDright / currentPIDleft * currentPIDright / currentPIDleft;
-                float newPwmRight = currentPwmRight * currentPIDleft / currentPIDright * currentPIDleft / currentPIDright;
-                newPwmLeft = (currentPwmLeft + newPwmLeft)/2;
-                newPwmRight = (currentPwmRight + newPwmRight)/2;
-                debug += "new pwm left: " + String(newPwmLeft);
-                debug += " new pwm right: " + String(newPwmRight); 
-                // std::cout << "before drive func: " << pulsesPerSec / abs(currentEncoderLeft) * currentPwmLeft << ", " << pulsesPerSec << ", " << abs(currentEncoderLeft) << ", " << currentPwmLeft << std::endl;
-                drive(newPwmLeft, newPwmRight);
-                lastEncLeft = getEncoderLeft();
-                lastEncRight = getEncoderRight();
-                // updatePosition(currentPIDleft, currentPIDright);
-                getData();
-            }
-            counter = 0;
-            getData();
-        }
-        delay(5);
-        updatePositionThread();
-        getData();
-    }
-    drive(0, 0); // stop motor
-    // updatePosition(currentPIDleft, currentPIDright);
+  target[0] = pulsesValue * distance;
+  target[1] = pulsesValue * distance;
 }
 
+void turnAngle(int degree) {
+  resetPosition();
 
+  int distance = wheelDistance * M_PI / 360 * degree;
+  target[0] = -pulsesValue * distance;
+  target[1] = pulsesValue * distance;
+}
 
-
-
-
-
-
-
+// Serial Communication
 
 void getData() { // get the data and run the actions
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     char command = input.charAt(0);
+
     if (command == 's') {
-      stopMotor();
+      resetPosition();
     } else if (command == 'd') {
       String valueStr = input.substring(2);
       int distance = valueStr.toInt();
       driveDistance(distance);
     } else if (command == 'w') {
-      // leftTriggered = false;
-      // rightTriggered = false;
+      leftTriggered = false;
+      rightTriggered = false;
       driveDistance(3000);
     } else if (command == 't') {
       String valueStr = input.substring(2);
       float angle = valueStr.toFloat();
-      turn(angle);
+      turnAngle(angle);
     } else if (command == 'g') {
-      // String valueStr = input.substring(2);
-      // int speed = valueStr.toInt();
-      // currentPwm = speed < pwmMax ? speed : pwmMax;
+      String valueStr = input.substring(2);
+      int speed = valueStr.toInt();
+      currentPwm = speed < pwmMax ? speed : pwmMax;
     }
   }
 }
 
 void sendData() {
   String data;
-  data += driving? "d" : "s";
+  data += isDriving ? "d" : "s";
   data += "x";
-  data += String(x);
+  data += String(x + extrax);
   data += "y";
-  data += String(y);
+  data += String(y + extray);
   data += "t";
-  data += String(theta);
-  data += " DEBUG ";
-  data += debug;
-  debug = "";
+  data += String(theta + extraTheta);
+  DEBUG += "Theta: " + String(theta * 180 / PI);
+  DEBUG += " ExtraTheta: " + String(extraTheta * 180 / PI);
+  data += " ";
+  data += DEBUG;
   Serial.println(data);
 }
 
+// Position update function
+
+void updatePosition() {
+  int pos[NMOTORS];
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    for (int k = 0; k < NMOTORS; k++) {
+      pos[k] = posi[k];
+    }
+  }
+
+  float leftEncChange = pos[1] - lastPos[1];
+  float rightEncChange = pos[0] - lastPos[0];
+  lastPos[0] = pos[0];
+  lastPos[1] = pos[1];
+
+  float leftDistance = pos[1] / pulsesPerMM;
+  float rightDistance = pos[0] / pulsesPerMM;
+  float distance = (leftDistance + rightDistance) / 2;
+  float dTheta = (leftDistance - rightDistance) / wheelDistance;
+  extrax = distance * cos(theta + dTheta);
+  extray = distance * sin(theta + dTheta);
+  extraTheta = dTheta;
+  // extraTheta = fmod((extraTheta + 2 * M_PI), (2 * M_PI)); // test in radian
+  while (extraTheta > 2 * M_PI) {
+    extraTheta -= 2 * M_PI;
+  }
+  while (extraTheta < -2 * M_PI) {
+    extraTheta += 2 * M_PI;
+  }
+
+  int maxD = fabs(target[0] - pos[0]);
+  maxD = maxD < fabs(target[1] - pos[1]) ? fabs(target[1] - pos[1]) : maxD;
+  if (fabs(leftEncChange) < pulsesCutoff &&
+      fabs(rightEncChange) < pulsesCutoff && maxD < 10) {
+    isDriving = false;
+    target[0] = pos[0];
+    target[1] = pos[1];
+  } else {
+    isDriving = true;
+  }
+
+  sendData();
+}
+
+// Setup function
+
 void setup() {
   Serial.begin(115200);
-  // encoder
+
   pinMode(LEFT_ENC_A_PHASE, INPUT_PULLUP);
   pinMode(LEFT_ENC_B_PHASE, INPUT_PULLUP);
   pinMode(RIGHT_ENC_A_PHASE, INPUT_PULLUP);
@@ -302,17 +251,76 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_B_PHASE), bi0, RISING);
   attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_A_PHASE), bi1, RISING);
 
-  // dc
-  pinMode(LEFT_LPWM, OUTPUT);
-  pinMode(LEFT_RPWM, OUTPUT);
-  pinMode(RIGHT_LPWM, OUTPUT);
-  pinMode(RIGHT_RPWM, OUTPUT);
+  target[0] = 0;
+  target[1] = 0;
+
+  for (int k = 0; k < NMOTORS; k++) {
+    pid[k].setParams(1, 0, 0, 100);
+  }
+
+  lastPosUpdate = micros();
 }
 
 // Loop function
 
 void loop() {
-  updatePositionThread(); // NEEDS TO RUN AS OFTEN AS POSSIBLE
-  getData(); // get all data and process stuff
-  sendData(); 
+  getData();
+  DEBUG = "";
+  DEBUG += "posi 0: ";
+  DEBUG += posi[0];
+  DEBUG += " posi 1: ";
+  DEBUG += posi[1];
+  DEBUG += " target 0: ";
+  DEBUG += target[0];
+  DEBUG += " target 1: ";
+  DEBUG += target[1];
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float)(currT - prevT)) / (1.0e6);
+  prevT = currT;
+
+  // Read the position in an atomic block to avoid a potential misread
+  int pos[NMOTORS];
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    for (int k = 0; k < NMOTORS; k++) {
+      pos[k] = posi[k];
+    }
+  }
+
+  // Update Changed Position
+  // alle 50ms
+  if (currT - lastPosUpdate >= 50000) {
+    updatePosition();
+    lastPosUpdate = currT;
+  }
+
+  if (!leftTriggered && true) { // && limit switch triggered
+    target[1] = pos[1];
+  }
+
+  if (!rightTriggered && true) { // && limit switch triggered
+    target[0] = pos[0];
+  }
+
+  int pwm[NMOTORS];
+  int dir[NMOTORS];
+  float scaledFactor[NMOTORS];
+  // loop through the motors
+  for (int k = 0; k < NMOTORS; k++) {
+    // evaluate the control signal
+    pid[k].evalu(pos[k], target[k], deltaT, pwm[k], dir[k]);
+    scaledFactor[k] = (float)pwm[k] / currentPwm;
+  }
+  float maxFactor =
+      scaledFactor[0] < scaledFactor[1] ? scaledFactor[1] : scaledFactor[0];
+  if (maxFactor > 1) {
+    pwm[0] /= maxFactor;
+    pwm[1] /= maxFactor;
+    Serial.println("Pwm 0: " + String(pwm[0]) + " Pwm 1: " + String(pwm[1]));
+  }
+
+  for (int k = 0; k < NMOTORS; k++) {
+    setMotor(dir[k], pwm[k], lpwm[k], rpwm[k]);
+  }
 }
