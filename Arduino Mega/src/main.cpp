86a10405772c87ebf851c7719907926de2384d91
Emanuel Berger <emanuelberger49@gmail.com>
2024-04-29 18:14:13 +0200

    supi codi

--- Arduino Mega/src/main.cpp
+++ Arduino Mega/src/main.cpp
@@ -269,7 +269,7 @@ void setup() {
 
   for (int k = 0; k < NMOTORS; k++) {
     // pid[k].setParams(0.7, 0.2, 0.05, 100);
-    pid[k].setParams(0.7, 0.005, 0.0, 100);
+    pid[k].setParams(0.7, 0.0025, 0.0, 100);
   }
 
   lastPosUpdate = micros();
@@ -346,5 +346,5 @@ void loop() {
     }
   }
   lastpwm = max(pwm[0], pwm[1]);
-  Serial.println(pwm[0] + "_" + pwm[1]);
+  //Serial.println(pwm[0] + "_" + pwm[1]);
 }
